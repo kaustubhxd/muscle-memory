95ae264d5be4432faf9028853d448f41cc16393d
Kaustubh Bhagwat <kaustubhpb@gmail.com>
2023-02-13 13:01:38 +0530

    Update axiosClient.js

--- src/helpers/axiosClient.js
+++ src/helpers/axiosClient.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const EXERCISES_URL = "https://api.api-ninjas.com/v1/exercises"
 const REACT_APP_EXCERCISES_API_KEY = process.env.REACT_APP_EXCERCISES_API_KEY
 
+console.log(REACT_APP_EXCERCISES_API_KEY)
+
 const client = axios.create({
     baseURL: EXERCISES_URL,
     // timeout: 1000,
